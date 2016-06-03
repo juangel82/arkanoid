1adbf585077cf1c8cfaf34c42e3dd3ec0116724e
juangel82 <juanillo82@gmail.com>
2016-06-03 11:48:37 +0200

    Update index.js

--- app/views/index.js
+++ app/views/index.js
@@ -6,13 +6,12 @@ define(['backbone', 'text!templates/inicio.html'], function(Backbone, html) {
     render: function render(modelOptions) {
       this.model = modelOptions;
       this.$el.html(html);
-      var tonto = new Media('file:///android_asset/www/assets/sound/musica.mp3');
-      console.log('tonto ' + tonto);
-      tonto.play();
-      if (this.model.get('music')){
-       var prueba = this.$('#musica');//.trigger('play');
-       console.log('esta'+prueba.getAttribute('src'));
-      }
+      
+       document.getElementById('musica').play();
+      
+        if (this.model.get('music')){
+        this.$('#musica').trigger('play');
+        }
       else {
         this.$('#musica').trigger('pause');
       }
