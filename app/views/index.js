1e12a44b8adb9b49d1bc2904bf7d3a50dbfea1cf
juangel82 <juanillo82@gmail.com>
2016-06-03 10:17:32 +0200

    Update index.js

--- app/views/index.js
+++ app/views/index.js
@@ -8,7 +8,8 @@ define(['backbone', 'text!templates/inicio.html'], function(Backbone, html) {
       this.$el.html(html);
 
       if (this.model.get('music')){
-        this.$('#musica').trigger('play');
+       var prueba = this.$('#musica');//.trigger('play');
+       console.log('esta'+prueba.getAttribute('src'));
       }
       else {
         this.$('#musica').trigger('pause');
