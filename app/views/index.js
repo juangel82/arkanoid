5c8b667f9d782f30c3058880f30201c71b041f84
juangel82 <juanillo82@gmail.com>
2016-06-03 13:45:50 +0200

    Update index.js

--- app/views/index.js
+++ app/views/index.js
@@ -10,7 +10,7 @@ define(['backbone', 'text!templates/inicio.html'], function(Backbone, html) {
        var tolo = document.getElementById('musica');
        var src = tolo.getAttribute('rsc');
        
-       if (tolo.src===""|| tolo.src===null) {
+       if (tolo.src==="") {
          console.log('es nulo');
          var media = new Media("file:///android_asset/www/assets/sound/musica.mp3");
          media.play();
